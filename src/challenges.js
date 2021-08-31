5514372a77c3105f809f10af2c8c5bb831b94eb9
Natã Elienai <nata.elienai@gmail.com>
2021-08-31 19:47:38 -0300

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -77,11 +77,47 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encodeChar(char) {
+  let encoding = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  if (encoding[char] !== undefined) {
+    return encoding[char];
+  }
+  return char;
 }
-function decode() {
-  // seu código aqui
+
+function decodeChar(char) {
+  let encoding = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  if (encoding[char] !== undefined) {
+    return encoding[char];
+  }
+  return char;
+}
+
+function encode(string) {
+  let encodedString = '';
+  for (let char of string) {
+    encodedString += encodeChar(char);
+  }
+  return encodedString;
+}
+function decode(string) {
+  let decodedString = '';
+  for (let char of string) {
+    decodedString += decodeChar(char);
+  }
+  return decodedString;
 }
 
 module.exports = {
